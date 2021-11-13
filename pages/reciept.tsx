7f6954cf098fcc37f3c634d5207d72930d7878a2
trexrush <edmazuera@gmail.com>
2021-11-13 16:53:27 -0600

    merged flask api and frontend

--- pages/reciept.tsx
+++ pages/reciept.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from 'react'
 import type { NextPage } from 'next'
 import Navbar from '../components/Navbar'
+import axios from 'axios'
 
+
+
+// localhost:5000/api/v1/add
 const Reciepts: NextPage = () => {
+  // postImage = useEffect = () => {
+  //   let formData = new FormData();
+  //   let imagefile = document.querySelector('#file');
+  //   formData.append("image", imagefile.files[0]);
+  //   axios.post('upload_file', formData, {
+  //     headers: {
+  //       'Content-Type': 'multipart/form-data'
+  //     }
+  // }
+  // }
   return (
     <>
       <Navbar/>
@@ -9,5 +24,7 @@ const Reciepts: NextPage = () => {
     </>
   )
 }
+
+
 
 export default Reciepts
