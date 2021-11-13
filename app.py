1582ec41d88a939d3fe1233f0d7b2317e6e4bffe
khreate <surajkhosla79@gmail.com>
2021-11-13 15:39:28 -0600

    made a POST route  where you can send receipt images

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return ('<p>Try /api/v1/endpoint next time.</p>')
+
+# route that receives a POST request containing an image
+@app.route('/api/v1/', methods=['POST'])
+def endpoint():
+    # get the image from the request
+    receipt = request.files['image']
+    # save the image to disk
+    receipt.save(f'images/{receipt.filename}')
+    # return a response
+    return jsonify({'message': 'Image received. Check /images/receipt.jpg for your receipt.'})
