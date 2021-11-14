fb3b57d6028cd43b795badff3725bbcb104082b5
trexrush <edmazuera@gmail.com>
2021-11-13 18:18:00 -0600

    faled attempt at figuring out how to upload reciept

--- pages/reciept.tsx
+++ pages/reciept.tsx
@@ -2,24 +2,33 @@ import { useEffect } from 'react'
 import type { NextPage } from 'next'
 import Navbar from '../components/Navbar'
 import axios from 'axios'
+import 'react-dropzone-uploader/dist/styles.css'
+import Dropzone from 'react-dropzone-uploader'
+
+const ImageAudioVideo = () => {
+  return (
+    <Dropzone
+      getUploadParams={getUploadParams}
+      onChangeStatus={handleChangeStatus}
+      onSubmit={handleSubmit}
+      accept="image/*"
+      inputContent={(files, extra) => (extra.reject ? 'Image, audio and video files only' : 'Drag Files')}
+      styles={{
+        dropzoneReject: { borderColor: 'red', backgroundColor: '#DAA' },
+        inputLabel: (files, extra) => (extra.reject ? { color: 'red' } : {}),
+      }}
+    />
+  )
+}
 
 
 
 // localhost:5000/api/v1/add
 const Reciepts: NextPage = () => {
-  // postImage = useEffect = () => {
-  //   let formData = new FormData();
-  //   let imagefile = document.querySelector('#file');
-  //   formData.append("image", imagefile.files[0]);
-  //   axios.post('upload_file', formData, {
-  //     headers: {
-  //       'Content-Type': 'multipart/form-data'
-  //     }
-  // }
-  // }
   return (
     <>
       <Navbar/>
+      <ImageAudioVideo />
       <div>Reciepts</div>
     </>
   )
