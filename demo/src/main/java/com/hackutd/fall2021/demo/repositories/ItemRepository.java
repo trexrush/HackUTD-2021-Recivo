c74e934ec51f65f12e19e0922ef7c3c1531b7434
Akihiro Yoshimoto <ayoshi0221@gmail.com>
2021-11-13 18:50:36 -0600

    Changed ReceiptRepo and ItemRepo generics

--- demo/src/main/java/com/hackutd/fall2021/demo/repositories/ItemRepository.java
+++ demo/src/main/java/com/hackutd/fall2021/demo/repositories/ItemRepository.java
@@ -3,10 +3,10 @@ package com.hackutd.fall2021.demo.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.hackutd.fall2021.demo.entities.User;
+import com.hackutd.fall2021.demo.entities.*;
 
 @Repository 
 
-public interface ItemRepository extends CrudRepository<User, Long> {
+public interface ItemRepository extends CrudRepository<Item, Long> {
 	
 }
--- demo/src/main/java/com/hackutd/fall2021/demo/repositories/ReceiptRepository.java
+++ demo/src/main/java/com/hackutd/fall2021/demo/repositories/ReceiptRepository.java
@@ -3,10 +3,13 @@ package com.hackutd.fall2021.demo.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hackutd.fall2021.demo.entities.Receipt;
 import com.hackutd.fall2021.demo.entities.User;
+
+import java.util.*;
 
 @Repository 
 
-public interface ReceiptRepository extends CrudRepository<User, Long> {
-	
+public interface ReceiptRepository extends CrudRepository<Receipt, Long> {
+	List<Receipt> findByUserAndDateGreaterAndDateLess(User user, Date after, Date before);
 }
